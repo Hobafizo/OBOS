a45bd40fcd0d53ed190b3fd70ae3b501c6f797d0
CoverBasha <ahmedgerab7@gmail.com>
2023-12-08 15:36:57 +0200

    Added Class Files

---
+++ OBOS/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBOS.Models
+{
+    public class Category
+    {
+        private string Name { get; set; }
+        private DateTime CreationDate { get; set; }
+
+        Order order;
+        public Category()
+        {
+            
+        }
+    }
+}
---
+++ OBOS/Models/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBOS.Models
+{
+    public class Order
+    {
+        private int Id { get; set; }
+        private User Customer { get; set; }
+        private List<KeyValuePair<Book, int>> Items { get; set; }
+        private int Quantity { get; set; }
+        private float TotalPrice { get; set; }
+        private DateTime Date { get; set; }
+
+        public float Total()
+        {
+            TotalPrice = 0;
+            foreach(var item in Items)
+            {
+                TotalPrice += item.Key.Price * (float)item.Value;
+            }
+            return TotalPrice;
+        }
+    }
+}
