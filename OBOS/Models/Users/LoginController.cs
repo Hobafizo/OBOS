d42d855765eae0b9c3279ba98379b4c53fc24a3e
Muhab <mohabfawzy2@gmail.com>
2023-12-09 03:22:18 +0200

    Added login controller

---
+++ OBOS/Models/Users/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBOS.Models.Users
+{
+	public class LoginController
+	{
+		public static LoginController Controller;
+
+		public User CurrentUser { get; private set; }
+
+		private LoginController()
+		{
+			Controller = null;
+			CurrentUser = null;
+		}
+
+		public bool Login(string username, string pw)
+		{
+			return false;
+		}
+
+		public bool Register(string username, string pw, string confirmpw, string address, string phone, bool isadmin = false)
+		{
+			return false;
+		}
+
+		public bool Logout()
+		{
+			return false;
+		}
+
+		public static LoginController GetInstance()
+		{
+			if (Controller == null)
+				Controller = new LoginController();
+			return Controller;
+		}
+	}
+}
--- OBOS/Models/Users/User.cs
+++ OBOS/Models/Users/User.cs
@@ -11,32 +11,13 @@ namespace OBOS.Models.Users
 {
     public abstract class User
     {
-        public static IdCounter = 1 ;
-        public int Id { get; set; };
-        public string UserName { get; set; };
-        public string Password { get; set; };
-        public string Address { get; set; };
-        public int Phone { get; set; };
-        public Boolean IsAdmin { get; set; };
+        public static int IdCounter = 1;
 
-		public bool Login(string username, string pw)
-        {
-            return false;
-        }
-
-        public bool Register(string UserName, string Password, string Address, string Phone, bool isadmin = false)
-        {
-            this.Id = IdCounter;
-            this.UserName = UserName;
-            this.Password = Password;
-            this.Address = Address; 
-            this.Phone = Phone;
-            Shop.GetInstance();
-        }
-
-        public bool Logout()
-        {
-            return false;
-        }
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public int Phone { get; set; }
+        public bool IsAdmin { get; set; }
     }
 }
