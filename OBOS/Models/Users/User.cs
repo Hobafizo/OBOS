769c381a8fabbecd153ef5e2edaa24f4323af4b8
Bahaa-Elnakib <bahaaelnakibb@gmail.com>
2023-12-09 03:19:27 +0200

    user after change

--- OBOS/Models/Users/User.cs
+++ OBOS/Models/Users/User.cs
@@ -4,20 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using OBOS.Models.Store;
+
 namespace OBOS.Models.Users
+
 {
     public abstract class User
     {
-		// <<<< Attributes here >>>>
+        public static IdCounter = 1 ;
+        public int Id { get; set; };
+        public string UserName { get; set; };
+        public string Password { get; set; };
+        public string Address { get; set; };
+        public int Phone { get; set; };
+        public Boolean IsAdmin { get; set; };
 
 		public bool Login(string username, string pw)
         {
             return false;
         }
 
-        public bool Register(string username, string pw, string confirmpw, string address, string phone, bool isadmin = false)
+        public bool Register(string UserName, string Password, string Address, string Phone, bool isadmin = false)
         {
-            return false;
+            this.Id = IdCounter;
+            this.UserName = UserName;
+            this.Password = Password;
+            this.Address = Address; 
+            this.Phone = Phone;
+            Shop.GetInstance();
         }
 
         public bool Logout()
